40c745e04235cb36006ec214027882345250a3ca
MargretraniF <margretrani.f@rubixtek.com>
2024-11-10 23:53:07 +0530

    complete web design

--- Demo/McloudTek/src/app/pages/menu/menu.component.ts
+++ Demo/McloudTek/src/app/pages/menu/menu.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrderDetailsService } from 'src/app/services/order-details.service';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+foodData:any;
+  constructor(private _orderservice:OrderDetailsService){
+
+  }
+  ngOnInit(): void {
+    this.foodData=this._orderservice.fooddetails;
+  }
 
 }
